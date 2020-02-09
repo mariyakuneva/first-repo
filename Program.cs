266d5d788c9479928dd3edd21e0b5de28e72e2d1
mariyakuneva <60814600+mariyakuneva@users.noreply.github.com>
2020-02-09 11:54:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int n;
+            while (a != 0)
+            {
+                a = a / 2;
+                n = a%2;
+                Console.Write(n);
+            }
+
+            Console.WriteLine();
+            int n1;
+            while (b != 0)
+            {
+                b = b / 2;
+                n1 = b % 2;
+                Console.Write(n1);
+            }
+            Console.WriteLine();
+            
+
+
+
+
+        }
+    }
+}
